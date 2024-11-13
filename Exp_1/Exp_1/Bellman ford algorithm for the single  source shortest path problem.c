35127e46fb6e04ec35eccb356fc3b2057de32a8f
AyushSingh2024 <160060773+AyushSingh2024@users.noreply.github.com>
2024-11-13 09:17:53 +0530

    Create Bellman ford algorithm for the single  source shortest path problem.c

---
+++ Exp_1/Exp_1/Bellman ford algorithm for the single  source shortest path problem.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+struct Edge {
+    int src, dest, weight;
+};
+
+void bellmanFord(struct Edge edges[], int V, int E, int src) {
+    int dist[V];
+
+    for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX;
+    dist[src] = 0;
+
+    for (int i = 1; i <= V - 1; i++) {
+        for (int j = 0; j < E; j++) {
+            int u = edges[j].src;
+            int v = edges[j].dest;
+            int weight = edges[j].weight;
+            if (dist[u] != INT_MAX && dist[u] + weight < dist[v])
+                dist[v] = dist[u] + weight;
+        }
+    }
+
+    // Check for negative-weight cycles
+    for (int j = 0; j < E; j++) {
+        int u = edges[j].src;
+        int v = edges[j].dest;
+        int weight = edges[j].weight;
+        if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
+            printf("Graph contains negative weight cycle\n");
+            return;
+        }
+    }
+
+    printf("Vertex \t Distance from Source\n");
+    for (int i = 0; i < V; i++)
+        printf("%d \t %d\n", i, dist[i]);
+}
